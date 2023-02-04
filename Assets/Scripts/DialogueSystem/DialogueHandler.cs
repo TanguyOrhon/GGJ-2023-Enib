d1c01b5a5b0e7a5f1526068df9ab3f5f7aa96647
figo711 <41550498+figo711@users.noreply.github.com>
2023-02-04 14:34:29 +0100

    Added DialogueSystem

---
+++ Assets/Scripts/DialogueSystem/DialogueHandler.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class DialogueHandler : MonoBehaviour
+    {
+        [SerializeField] private DialogueUI dialogueUI;
+        [SerializeField] private List<DialogueSequence> sequences;
+
+        private int _currentSequenceIndex; private DialogueSequence _currentSequence;
+        private DialogueData _currentSpeaker; private int _currentSentenceIndex;
+        private int _currentSubsentenceIndex;
+
+        private void Awake()
+        {
+            _currentSequenceIndex = -1;
+            _currentSequence = null;
+            _currentSpeaker = null;
+            _currentSentenceIndex = -1;
+            _currentSubsentenceIndex = -1;
+        }
+
+        private void Start()
+        {
+            LoadSequence();
+        }
+
+        public void LoadSequence()
+        {
+            NextSequence(); // the first by default
+        }
+
+        private void NextSentence()
+        {
+            _currentSentenceIndex++;
+            if (_currentSentenceIndex >= _currentSequence.DialogueList.Count)
+            {
+                _currentSentenceIndex = -1;
+                // On finished
+                NextSequence();
+                return;
+            }
+            _currentSpeaker = _currentSequence.DialogueList[_currentSentenceIndex];
+            NextSubSentence();
+        }
+
+        private void NextSubSentence()
+        {
+            _currentSubsentenceIndex++;
+            if (_currentSubsentenceIndex >= _currentSpeaker.Sentences.Count)
+            {
+                _currentSubsentenceIndex = -1;
+                // On finished
+                NextSentence();
+                return;
+            }
+
+            var s = _currentSpeaker.Sentences[_currentSubsentenceIndex];
+            dialogueUI.StopAllTypings();
+            dialogueUI.UpdateBox(_currentSpeaker.Speaker, s);
+        }
+
+        private void NextSequence()
+        {
+            _currentSequenceIndex++;
+            if (_currentSequenceIndex >= sequences.Count)
+            {
+                _currentSequenceIndex = -1;
+                // On finished
+                dialogueUI.HideDialogue();
+                print("SEQUENCES END");
+                return;
+            }
+            _currentSequence = sequences[_currentSequenceIndex];
+            NextSentence();
+        }
+
+        public void NextButtonAction()
+        {
+            NextSubSentence();
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueSystem/DialogueUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI nameDisplay;
+        [SerializeField] private TextMeshProUGUI textDisplay;
+
+        public void StopAllTypings()
+        {
+            StopAllCoroutines();
+        }
+
+        public void UpdateBox(string _name, string text)
+        {
+            nameDisplay.text = _name;
+            StartCoroutine(TypingText(text, 0.025f));
+        }
+
+        private IEnumerator TypingText(string text, float delay)
+        {
+            textDisplay.text = "";
+            foreach (var c in text)
+            {
+                textDisplay.text += c;
+                yield return new WaitForSecondsRealtime(delay);
+            }
+        }
+
+        public void HideDialogue()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
