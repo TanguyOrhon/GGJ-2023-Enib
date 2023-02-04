5c4175061d47f578d757e2332ead48f659d4004b
TanguyOrhon <tanguy.orhon32@gmail.com>
2023-02-04 16:01:18 +0100

    Added Script Innocent

---
+++ Assets/Scripts/Innocent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Innocent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<string> caracter = new List<string>();
+    
+    void Start()
+    {
+        caracter.Add("Tim");
+        caracter.Add("Gwenn");
+        caracter.Add("Maya");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
