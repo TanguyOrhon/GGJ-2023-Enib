eb37fe9f12d025d19dcf064af3e215774e67c5ed
TanguyOrhon <tanguy.orhon32@gmail.com>
2023-02-05 10:55:24 +0100

    Update EndLoose.cs

--- Assets/Scripts/EndLoose.cs
+++ Assets/Scripts/EndLoose.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,7 +9,7 @@ using UnityEngine.UI;
 public class EndLoose : MonoBehaviour
 {
 
-    public Image text_button;
+    public TextMeshProUGUI text_button;
     
     // Start is called before the first frame update
     void Start()
